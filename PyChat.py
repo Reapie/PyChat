9258e02493ad54d8ccfc4b9171f6e5766048019a
Martin Juritsch <63295801+Reapie@users.noreply.github.com>
2020-08-26 13:54:05 +0200

    Update PyChat.py

--- PyChat.py
+++ PyChat.py
@@ -9,7 +9,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-password = "Test"
+password = ""
 PORT = 42069
 SIZE = 1024 * 5
 wanted_nick = ""
